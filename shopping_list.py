d3916ec3970db4dd9e8e13365689bd140624607b
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-03-14 20:39:20 -0700

    adding ability to show list once the user enters DONE

--- shopping_list.py
+++ shopping_list.py
@@ -1,16 +1,25 @@
 
 shopping_list = []
-def add_to_list(item):
-  shopping_list.append(item)
-  print("Added! List has {} items.".format(len(shopping_list)))
 
 def show_help():
   print("What should we pick up at the store?")
   print("""
   Enter 'DONE' to stop adding items. 
-  Enter 'HELP' for this help
+  Enter 'HELP' for this help.
+  Enter 'SHOW' to see your current list.
   """)
   
+
+def add_to_list(item):
+  shopping_list.append(item)
+  print("Added! List has {} items.".format(len(shopping_list)))
+
+# define a  new fn called show list  that prints all items in the list 
+def show_list():
+  print("Here is your list:")
+  for item in shopping_list:
+    print(item)
+
 show_help()
 while True: 
   new_item = input("> ")
@@ -18,6 +27,13 @@ while True:
   if new_item == 'DONE':
     break
   elif new_item == 'HELP':
+    continue
+  elif new_item == 'SHOW':
     show_help()
     continue
+    # enable the show command. 
+  # HINT: make sure to run  it
+  show_list()
   add_to_list(new_item)
+
+show_list()
