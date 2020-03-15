76d41f1cfc02e5f2cfaf2655d7415ccea8786d9f
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-03-14 20:24:58 -0700

    adding a function that adds item to list and notification when item is added

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,13 @@
+# create a new empty list called shopping list
+shopping_list = []
+#create a fn named add to list that declares a parameter named item 
+def add_to_list(item):
+  #add the item to the list
+  shopping_list.append(item)
+  # notify the user that the item was added and state the number of items in the current list
+  print("Added! List has {} items.".format(len(shopping_list)))
+
+
 def show_help():
   print("What should we pick up at the store?")
   print("""
@@ -6,8 +16,6 @@ def show_help():
   """)
   
 show_help()
-
-
 while True: 
   new_item = input("> ")
 
@@ -16,3 +24,5 @@ while True:
   elif new_item == 'HELP':
     show_help()
     continue
+    # call add_to_list with new item as an argument
+    add_to_list(new_item)
